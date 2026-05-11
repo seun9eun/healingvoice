495b37684cb236a5405085840208c4ad2fd44b0b
Jeong Cheon uk <interstander@cgnmail.net>
2026-05-11 17:10:40 +0900

    지원요건 UI 미세 조정2

--- src/app/translations.ts
+++ src/app/translations.ts
@@ -75,7 +75,7 @@ export const translations = {
         title: "지원 요건",
         desc: "CCM을 사랑하는 기독교인 누구나 지원 가능!\n세상을 향한 당신의 목소리를 들려주세요.",
         item1: {
-          title: "대중 가수\u00A0/ CCM 가수\u00A0/\n찬양 사역자",
+          title: "대중\u00A0가수 / CCM 가수 / 찬양\u00A0사역자",
           desc: "",
         },
         item2: { title: "CCM을\n 사랑하는 사람", desc: "" },
